8c27a224f30af9bca4a0276229dccd11f0b561b5
Harold Tenorio <harold.tenorio@epn.edu.ec>
2021-11-16 15:37:39 -0500

    Termianmos todas las herramientas aritmeticas y le daremos paso a los estilos

--- promedio.js
+++ promedio.js
@@ -1,9 +1,19 @@
 
+
+function CalcularMediab(){
+    const numbers = document.getElementById("inputM").value;
+    let listanumber = [];
+    listanumber = numbers.split(",");
+    const listanumber2 = listanumber.map(x => Number(x))
+    const resultado = calcularMedia(listanumber2);
+    const resultP = document.getElementById("ResultP")               
+    resultP.innerHTML = "La media es: " + resultado;                                               
+}   
 function calcularMedia(lista) {
     // let sumaLista = 0;
     // for (let i = 0; i < lista1.length; i++) {
     //     sumaLista = sumaLista + lista[i];
-    // }
+    // }  
     const sumaLista = lista.reduce(
         function(valorAcumulado = 0, nuevoElemento){
             return valorAcumulado + nuevoElemento
@@ -16,7 +26,18 @@ function calcularMedia(lista) {
 
 // mediana
 
+function CalcularMedianab(){
+    const numbers = document.getElementById("inputM").value;
+    let listanumber = [];
+    listanumber = numbers.split(",");
+    const listanumber2 = listanumber.map(x => Number(x))
+    const resultado = calcularMediana(listanumber2);
+    const resultP = document.getElementById("ResultP")               
+    resultP.innerHTML = "La mediana es: " + resultado;                                               
+}
+
 function calcularMediana(lista) {
+    lista.sort(function(a,b){return b - a });
     
     const mitadListas1 = parseInt(lista.length / 2);
     
@@ -45,4 +66,41 @@ function calcularMediana(lista) {
 
 
 
+}
+
+
+function CalcularModab(){
+    const numbers = document.getElementById("inputM").value;
+    let listanumber = [];
+    listanumber = numbers.split(",");
+    const listanumber2 = listanumber.map(x => Number(x))
+    const resultado = calcularModa(listanumber2);
+    const resultP = document.getElementById("ResultP")               
+    resultP.innerHTML = "La moda es: " + resultado;                                               
+}
+
+
+
+function calcularModa(lista) {
+    const lista1Count = {};
+    
+    lista.map(
+        function(elemento) {
+            if( lista1Count[elemento]){
+                lista1Count[elemento] += 1;
+            } else{
+                lista1Count[elemento] = 1;
+            }
+        }
+    );
+    
+    const lista1Array = Object.entries(lista1Count).sort(
+        function(elementoA , elementoB){
+            return elementoA[1] - elementoB[1];
+        }
+    );
+    
+    const moda = lista1Array[lista1Array.length - 1];
+    const result = moda.shift();
+    return result
 }
